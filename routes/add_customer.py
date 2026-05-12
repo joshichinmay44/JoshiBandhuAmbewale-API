68d552e1cdd3b5404101a30e9498a015ba946052
joshichinmay44 <joshichinmay44@gmail.com>
2026-05-13 01:06:07 +0530

    intermediate changes for update customer

--- routes/add_customer.py
+++ routes/add_customer.py
@@ -14,13 +14,17 @@ conn = common_util.establish_connection_with_db()
 conn.autocommit = True
 
 @router.post("/add_customer")
-async def add_customer(first_name: str =Body(), last_name: str =Body(), email: str =Body(), phone_number_calling: str =Body(), phone_number_whatsapp: str =Body(), customer_type: str =Body(), customer_mode: str =Body(), created_by: str =Body()):
+async def add_customer(first_name: str =Body(), last_name: str =Body(), email: str =Body(), phone_number_calling: str =Body(), phone_number_whatsapp: str =Body(), customer_type: str =Body(), customer_mode: str =Body(), created_by: str =Body(), country: str =Body(), state: str =Body(), city: str =Body(), pincode: str =Body(), street: str = Body()):
     try:
         logging.info("Starting add_user process")
-        print("Received data:", first_name, last_name, email, phone_number_calling, phone_number_whatsapp, customer_type, customer_mode, created_by)
+        print("Received data:", first_name, last_name, email, phone_number_calling, phone_number_whatsapp, customer_type, customer_mode, created_by, country, state, city, pincode, street)
         username=created_by
         cursor = conn.cursor()
-        cursor.execute("CALL dim.usp_insert_new_customer(%s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar);", (first_name, last_name, email, phone_number_calling, phone_number_whatsapp, customer_type, customer_mode, created_by))
+        cursor.execute("SELECT customer_id FROM dim.customer WHERE first_name = %s AND (phone_number_calling=%s OR phone_number_whatsapp=%s);", (first_name, phone_number_calling, phone_number_whatsapp))
+        user_id = cursor.fetchone()
+        if user_id:
+            raise HTTPException(status_code=400, detail="User already exists")
+        cursor.execute("CALL dim.usp_insert_new_customer(%s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar);", (first_name, last_name, email, phone_number_calling, phone_number_whatsapp, customer_type, customer_mode, created_by, country, state, city, pincode, street))
         cursor.close()
         return {"message": "User added successfully", "status": "success"}
     except Exception as e:
--- routes/update_customer.py
+++ routes/update_customer.py
@@ -14,13 +14,13 @@ conn = common_util.establish_connection_with_db()
 conn.autocommit = True
 
 @router.post("/update_customer")
-async def update_customer(first_name: str =Body(), last_name: str =Body(), email: str =Body(), phone_number_calling: str =Body(), phone_number_whatsapp: str =Body(), customer_type: str =Body(), customer_mode: str =Body(), updated_by: str =Body(),cust_id: int =Body()):
+async def update_customer(first_name: str =Body(), last_name: str =Body(), email: str =Body(), phone_number_calling: str =Body(), phone_number_whatsapp: str =Body(), customer_type: str =Body(), customer_mode: str =Body(), updated_by: str =Body(),cust_id: int =Body(), country: str =Body(), state: str =Body(), city: str =Body(), pincode: str =Body(), street: str = Body()):
     try:
         logging.info("Starting add_user process")
         print("Received data:", first_name, last_name, email, phone_number_calling, phone_number_whatsapp, customer_type, customer_mode, updated_by)
         username=updated_by
         cursor = conn.cursor()
-        cursor.execute("CALL dim.usp_update_customer(%s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::bigint);", (first_name, last_name, email, phone_number_calling, phone_number_whatsapp, customer_type, customer_mode, updated_by, cust_id))
+        cursor.execute("CALL dim.usp_update_customer(%s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::varchar, %s::bigint, %s::varchar, %s::varchar, %s::varchar, %s::varchar);", (first_name, last_name, email, phone_number_calling, phone_number_whatsapp, customer_type, customer_mode, updated_by, cust_id, country, state, city, pincode, street))
         cursor.close()
         return {"message": "User updated successfully", "status": "success"}
     except Exception as e:
